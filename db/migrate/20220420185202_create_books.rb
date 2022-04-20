7e5690cc9814d4e8d7799e68a9d84b19ca05975c
santi <santiagodelafuente2013@gmail.com>
2022-04-20 15:54:19 -0300

    add users, authors and books

---
+++ db/migrate/20220420185202_create_books.rb
@@ -0,0 +1,9 @@
+class CreateBooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :books do |t|
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
