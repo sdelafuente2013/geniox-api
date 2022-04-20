9e997bea87b1c4cb785fcf6c22e9840b1f5aca2e
santi <santiagodelafuente2013@gmail.com>
2022-04-20 18:24:17 -0300

    add route '/users'

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # /api/v1/users
+  namespace :api, defaults: { format: 'json' } do
+    namespace :v1 do
+      # get 'users', to: 'users#index'
+      resources :users, only: [:index]
+    end
+  end
 end
